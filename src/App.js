9ae9164035df82cc381d332252540c437f59c102
Abesh <abeshsarkar2030@gmail.com>
2024-04-19 19:02:29 +0530

    restaurant page

---
+++ src/App.js
@@ -0,0 +1,59 @@
+
+import Layout from './Layout';
+import { HomeScreen } from './home/HomeScreen';
+import { BrowserRouter, Routes, Route, createBrowserRouter, RouterProvider } from "react-router-dom";   
+import { QuoteScreen } from './components/quoteScreen';
+import Restaurant from './restaurants/Restaurant';
+import Food from './Food';
+import Contact from './Contact';
+
+
+function App() {
+
+  const allPaths = createBrowserRouter([
+    
+    {
+      path: '/',
+      element: <Layout/>,
+      children:  [
+        {
+          path: '/',
+          element: <HomeScreen/>
+        },
+        {
+          path: 'quotes',
+          element: <QuoteScreen/>
+        },
+        {
+          path: 'restaurant',
+          element: <Restaurant />
+        },
+        {
+          path: 'food',
+          element: <Food />
+        },
+        {
+          path: 'contact',
+          element: <Contact />
+        }
+      ]
+    }
+  ])
+    
+  
+  return (
+    <>
+    {/* <BrowserRouter>
+      <Routes>
+      <Route path='/' element = { <HomeScreen/> } />
+      <Route path='/quotes' element = {<QuotePage/>} />
+      </Routes>
+      </BrowserRouter> */}
+
+      <RouterProvider router={allPaths} />
+    </>
+
+  );
+}
+
+export default App;
---
+++ src/Layout.jsx
@@ -0,0 +1,18 @@
+// import 
+import React from 'react'
+import { Navbar } from './components/Navbar'
+import { Footer } from './components/Footer'
+import { Outlet } from 'react-router'
+
+
+const Layout = () => {
+    return (
+        <>
+            <Navbar />
+            <Outlet />
+            <Footer />
+        </>
+    )
+}
+
+export default Layout
